bdfcedf75e5476f1d18886d909801edb526ed571
Claire Giordano <cgiordano12@fordham.edu>
2020-08-03 16:43:06 -0400

    Completed Products Table & Backend Routes

---
+++ server/db/models/product.js
@@ -0,0 +1,40 @@
+const Sequelize = require('sequelize')
+const db = require('../db')
+
+//Product Table
+const Product = db.define('product', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+
+  description: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+
+  price: {
+    type: Sequelize.FLOAT,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+
+  imageUrl: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+    defaultValue: 'https://i.ytimg.com/vi/_ph7TzyW318/maxresdefault.jpg',
+    validate: {
+      notEmpty: true
+    }
+  }
+})
+
+module.exports = Product
