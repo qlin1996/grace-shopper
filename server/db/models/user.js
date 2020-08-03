fbeeee2b74b0858681de6f1c589ed8efbce88c42
Simon Zeng <zeng.simonl@gmail.com>
2020-08-03 16:19:34 -0400

    Finished User Model

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -1,70 +1,90 @@
-const crypto = require('crypto')
-const Sequelize = require('sequelize')
-const db = require('../db')
+const crypto = require('crypto');
+const Sequelize = require('sequelize');
+const db = require('../db');
 
 const User = db.define('user', {
-  email: {
-    type: Sequelize.STRING,
-    unique: true,
-    allowNull: false
-  },
-  password: {
-    type: Sequelize.STRING,
-    // Making `.password` act like a func hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
-    get() {
-      return () => this.getDataValue('password')
-    }
-  },
-  salt: {
-    type: Sequelize.STRING,
-    // Making `.salt` act like a function hides it when serializing to JSON.
-    // This is a hack to get around Sequelize's lack of a "private" option.
-    get() {
-      return () => this.getDataValue('salt')
-    }
-  },
-  googleId: {
-    type: Sequelize.STRING
-  }
-})
+	email     : {
+		type      : Sequelize.STRING,
+		allowNull : false,
+		validate  : {
+			notEmpty : true,
+			isEmail  : true
+		}
+	},
+	password  : {
+		type : Sequelize.STRING,
+		// Making `.password` act like a func hides it when serializing to JSON.
+		// This is a hack to get around Sequelize's lack of a "private" option.
+		get() {
+			return () => this.getDataValue('password');
+		}
+	},
+	salt      : {
+		type : Sequelize.STRING,
+		// Making `.salt` act like a function hides it when serializing to JSON.
+		// This is a hack to get around Sequelize's lack of a "private" option.
+		get() {
+			return () => this.getDataValue('salt');
+		}
+	},
+	googleId  : {
+		type : Sequelize.STRING
+	},
+	address   : {
+		type      : Sequelize.TEXT,
+		allowNull : false,
+		validate  : {
+			notEmpty : true
+		}
+	},
+	firstName : {
+		type      : Sequelize.STRING,
+		allowNull : false,
+		validate  : {
+			notEmpty : true
+		}
+	},
+	lastName  : {
+		type      : Sequelize.STRING,
+		allowNull : false,
+		validate  : {
+			notEmpty : true
+		}
+	}
+});
 
-module.exports = User
+module.exports = User;
 
 /**
  * instanceMethods
  */
 User.prototype.correctPassword = function(candidatePwd) {
-  return User.encryptPassword(candidatePwd, this.salt()) === this.password()
-}
+	return User.encryptPassword(candidatePwd, this.salt()) === this.password();
+};
 
 /**
  * classMethods
  */
 User.generateSalt = function() {
-  return crypto.randomBytes(16).toString('base64')
-}
+	return crypto.randomBytes(16).toString('base64');
+};
 
 User.encryptPassword = function(plainText, salt) {
-  return crypto
-    .createHash('RSA-SHA256')
-    .update(plainText)
-    .update(salt)
-    .digest('hex')
-}
+	return crypto.createHash('RSA-SHA256').update(plainText).update(salt).digest('hex');
+};
 
 /**
  * hooks
  */
-const setSaltAndPassword = user => {
-  if (user.changed('password')) {
-    user.salt = User.generateSalt()
-    user.password = User.encryptPassword(user.password(), user.salt())
-  }
-}
+const setSaltAndPassword = (user) => {
+	if (user.changed('password')) {
+		user.salt = User.generateSalt();
+		user.password = User.encryptPassword(user.password(), user.salt());
+	}
+};
 
-User.beforeCreate(setSaltAndPassword)
-User.beforeUpdate(setSaltAndPassword)
-User.beforeBulkCreate(users => {
-  users.forEach(setSaltAndPassword)
-})
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
+User.beforeBulkCreate((users) => {
+	users.forEach(setSaltAndPassword);
+});
