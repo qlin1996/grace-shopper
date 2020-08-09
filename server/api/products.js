788d89f8df186986a7118880dec967f78a0d6132
Qi (Q) Lin <58145741+qlin1996@users.noreply.github.com>
2020-08-08 21:33:12 -0400

    Update products.js

--- server/api/products.js
+++ server/api/products.js
@@ -1,19 +1,8 @@
 const router = require('express').Router()
 const {Product} = require('../db/models')
 
-//proctection A.K.A. isAdmin
-const isAdmin = (req, res, next) => {
-  if (!User.user && !User.isAdmin) {
-    const error = new Error("you can't hack us")
-    res.status(401).send(error)
-    return next(error)
-  } else {
-    next()
-  }
-}
-
 //GET --> /api/products
-router.get('/', isAdmin, async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const products = await Product.findAll()
     res.json(products)
@@ -23,7 +12,7 @@ router.get('/', isAdmin, async (req, res, next) => {
 })
 
 //GET --> /api/products/:id
-router.get('/:id', isAdmin, async (req, res, next) => {
+router.get('/:id', async (req, res, next) => {
   try {
     const product = await Product.findOne({
       where: {
